f665d9abef968238a7cecff92687d4498bda7111
SujinKim-sj <sjsjkim0@gmail.com>
2022-01-19 17:50:03 +0900

    Update location

--- src/com/java/s1/start/StartMain.java
+++ src/com/java/s1/start/StartMain.java
@@ -5,6 +5,9 @@ import java.util.List;
 import com.java.s1.department.DepartmentDAO;
 import com.java.s1.department.DepartmentDTO;
 import com.java.s1.department.DepartmentView;
+import com.java.s1.location.LocationDAO;
+import com.java.s1.location.LocationDTO;
+import com.java.s1.location.LocationView;
 import com.java.s1.util.DBConnector;
 
 public class StartMain {
@@ -16,10 +19,16 @@ public class StartMain {
 		DepartmentDAO departmentDAO = new DepartmentDAO();
 		DepartmentView departmentView = new DepartmentView();
 		
+		LocationDAO locationDAO = new LocationDAO();
+		LocationView locationView = new LocationView();
+		
 		try {
 			//dbConnector.getConnect();
-			List<DepartmentDTO> ar = departmentDAO.getList();
-			departmentView.view(ar);
+			//List<DepartmentDTO> ar = departmentDAO.getList();
+			//departmentView.view(ar);
+			
+			List<LocationDTO> ar = locationDAO.getList();
+			locationView.view(ar);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
