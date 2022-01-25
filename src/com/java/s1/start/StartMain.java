5869cf80338feefe2cf2ce3fa6fca22453b098cd
SujinKim-sj <sjsjkim0@gmail.com>
2022-01-25 16:47:57 +0900

    Update Main

--- src/com/java/s1/start/StartMain.java
+++ src/com/java/s1/start/StartMain.java
@@ -1,14 +1,19 @@
 package com.java.s1.start;
 
+import com.java.s1.region.RegionController;
+
 public class StartMain {
 
 	public static void main(String[] args) {
 		System.out.println("DB 연동 테스트 시작");
 	
-		FrontController frontController = new FrontController();
+		//FrontController frontController = new FrontController();
+		RegionController regionController = new RegionController();
 		
 		try {
-			frontController.mainStart();
+			//frontController.mainStart();
+			regionController.start();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
